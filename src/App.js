068c60aa9dce909059e1fecdd43342c929eba252
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-01-27 20:17:58 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './components/About/About';
+import Inventory from './components/Inventory/Inventory';
+import Orders from './components/Orders/Orders';
+import Shop from './components/Shop/Shop';
+import Main from './Layout/Main';
+
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main ></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Shop></Shop>
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
+        },
+        {
+          path: '/inventory',
+          element: <Inventory></Inventory>
+        },
+        { path: '/about', element: <About></About> },
+      ]
+    },
+
+  ])
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 };
---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>about loading.................</h2>
+        </div>
+    );
+};
+
+export default About;
